5daab37d220b52dc22f92c1f2e0e4dd076aaa4cc
Alexander Makarov <sam@rmcreative.ru>
2020-11-16 21:27:37 +0300

    Fix not found handler

--- config/web/middleware-dispatcher.php
+++ config/web/middleware-dispatcher.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\NotFoundHandler;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Router\Middleware\Router;
 use Yiisoft\ErrorHandler\ErrorCatcher;
@@ -9,8 +10,9 @@ use Yiisoft\Yii\Web\Middleware\SubFolder;
 use Yiisoft\Yii\Web\MiddlewareDispatcher;
 
 return [
-    MiddlewareDispatcher::class => static fn (ContainerInterface $container) => (new MiddlewareDispatcher($container))
-        ->addMiddleware($container->get(Router::class))
-        ->addMiddleware($container->get(SubFolder::class))
-        ->addMiddleware(($container->get(ErrorCatcher::class))->forceContentType('application/json')),
+    MiddlewareDispatcher::class => static fn (ContainerInterface $container) =>
+        (new MiddlewareDispatcher($container, $container->get(NotFoundHandler::class)))
+            ->addMiddleware($container->get(Router::class))
+            ->addMiddleware($container->get(SubFolder::class))
+            ->addMiddleware(($container->get(ErrorCatcher::class))->forceContentType('application/json')),
 ];
