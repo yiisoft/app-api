0f2ddc9563ee9b3ff3c9df2fcc850d8cbd7709bc
romkatsu <r@tsurkanu.ru>
2020-12-06 22:57:11 +0300

    Add open api docs for blog controller (#27)

--- src/Blog/PageRequest.php
+++ src/Blog/PageRequest.php
@@ -5,7 +5,19 @@ declare(strict_types=1);
 namespace App\Blog;
 
 use Yiisoft\RequestModel\RequestModel;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Parameter(
+ *      @OA\Schema(
+ *          type="int",
+ *          example="2"
+ *      ),
+ *      in="query",
+ *      name="page",
+ *      parameter="PageRequest"
+ * )
+ */
 final class PageRequest extends RequestModel
 {
     private const DEFAULT_PAGE_PARAM = 1;
--- src/Formatter/PaginatorFormatter.php
+++ src/Formatter/PaginatorFormatter.php
@@ -5,7 +5,16 @@ declare(strict_types=1);
 namespace App\Formatter;
 
 use Yiisoft\Data\Paginator\OffsetPaginator;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *      schema="Paginator",
+ *      @OA\Property(example="10", property="pageSize", format="int"),
+ *      @OA\Property(example="1", property="currentPage", format="int"),
+ *      @OA\Property(example="3", property="totalPages", format="int"),
+ * )
+ */
 final class PaginatorFormatter
 {
     public function format(OffsetPaginator $paginator): array
