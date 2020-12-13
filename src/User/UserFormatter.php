abf6d17c32515d3a40853a6110bc6fdefb14943e
romkatsu <r@tsurkanu.ru>
2020-12-13 18:19:55 +0300

    Add open api docs

--- src/User/UserFormatter.php
+++ src/User/UserFormatter.php
@@ -3,7 +3,15 @@
 declare(strict_types=1);
 
 namespace App\User;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema(
+ *      schema="User",
+ *      @OA\Property(example="UserName", property="login", format="string"),
+ *      @OA\Property(example="13.12.2020 00:04:20", property="created_at", format="string"),
+ * )
+ */
 final class UserFormatter
 {
     public function format(User $user): array
