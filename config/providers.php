962b4405c9ed7c92336475b5243599414ed5e1fe
Alexander Makarov <sam@rmcreative.ru>
2021-01-30 23:38:46 +0300

    Update configs after moving array modifiers to config plugin

--- config/providers.php
+++ config/providers.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use App\Provider\RepositoryProvider;
-use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
+use Yiisoft\Composer\Config\Merger\Modifier\ReverseBlockMerge;
 
 return [
     RepositoryProvider::class => RepositoryProvider::class,
