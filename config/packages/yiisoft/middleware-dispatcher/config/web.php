832eedfa74cde339b35c9bc553e74945f3d36875
Alexander Makarov <sam@rmcreative.ru>
2021-03-22 01:28:09 +0300

    Fix middleware factory config

--- config/packages/yiisoft/middleware-dispatcher/config/web.php
+++ config/packages/yiisoft/middleware-dispatcher/config/web.php
@@ -2,12 +2,9 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Middleware\Dispatcher\MiddlewareFactory;
-use Yiisoft\Middleware\Dispatcher\MiddlewareFactoryInterface;
 use Yiisoft\Middleware\Dispatcher\MiddlewarePipelineInterface;
 use Yiisoft\Middleware\Dispatcher\MiddlewareStack;
 
 return [
     MiddlewarePipelineInterface::class => MiddlewareStack::class,
-    MiddlewareFactoryInterface::class => MiddlewareFactory::class,
 ];
