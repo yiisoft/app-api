69ef381c9a6387b6804581f08301f275d3d55989
Alexander Makarov <sam@rmcreative.ru>
2021-03-22 16:23:23 +0300

    Fix formatter

--- config/packages/yiisoft/data-response/config/web.php
+++ config/packages/yiisoft/data-response/config/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Formatter\ApiResponseFormatter;
 use Yiisoft\DataResponse\DataResponseFactory;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\DataResponse\DataResponseFormatterInterface;
@@ -13,7 +14,7 @@ use Yiisoft\DataResponse\Middleware\ContentNegotiator;
 /* @var $params array */
 
 return [
-    DataResponseFormatterInterface::class => HtmlDataResponseFormatter::class,
+    DataResponseFormatterInterface::class => ApiResponseFormatter::class,
     DataResponseFactoryInterface::class => DataResponseFactory::class,
     ContentNegotiator::class => [
         '__construct()' => [
