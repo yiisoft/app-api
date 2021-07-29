1dd0d2a9483ef9148ab19361c16036ce3a6a7883
Dmitry Derepko <xepozz@list.ru>
2021-07-30 02:12:27 +0300

    Move middleware to common config

--- config/web/application.php
+++ config/web/application.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 use App\NotFoundHandler;
 use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
+use Yiisoft\Factory\Definition\DynamicReference;
+use Yiisoft\Factory\Definition\Reference;
 use Yiisoft\Injector\Injector;
 use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
 use Yiisoft\Router\Middleware\Router;
+use Yiisoft\Yii\Sentry\SentryMiddleware;
 use Yiisoft\Yii\Web\Middleware\SubFolder;
-use Yiisoft\Factory\Definition\Reference;
-use Yiisoft\Factory\Definition\DynamicReference;
 
 return [
     Yiisoft\Yii\Web\Application::class => [
@@ -20,6 +21,7 @@ return [
                         [
                             Router::class,
                             SubFolder::class,
+                            SentryMiddleware::class,
                             ErrorCatcher::class,
                         ]
                     );
