26732cabb74fa02fe004d4fbc2590a97ade8f9a6
Daniel Gurev <doodka.dood@gmail.com>
2023-10-18 00:45:21 +0300

    Increase psalm level

--- tests/Unit/Http/ApiResponseDataFactoryTest.php
+++ tests/Unit/Http/ApiResponseDataFactoryTest.php
@@ -8,6 +8,7 @@ use App\Http\ApiResponseDataFactory;
 use HttpSoft\Message\ResponseFactory;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\StreamFactoryInterface;
+use RuntimeException;
 use Yiisoft\DataResponse\DataResponse;
 
 final class ApiResponseDataFactoryTest extends TestCase
@@ -73,5 +74,44 @@ final class ApiResponseDataFactoryTest extends TestCase
             'error_code' => 555,
             'data' => null,
         ], $response->toArray());
+    }
+
+    public function testGenericSuccessResponse(): void
+    {
+        $factory = new ApiResponseDataFactory();
+
+        $response = new DataResponse(
+            ['message' => 'success message'],
+            200,
+            'Testing phase',
+            new ResponseFactory(),
+            $this->createStub(StreamFactoryInterface::class),
+        );
+        $response = $factory->createFromResponse($response);
+
+        $this->assertEquals([
+            'status' => 'success',
+            'error_message' => '',
+            'error_code' => null,
+            'data' => ['message' => 'success message'],
+        ], $response->toArray());
+    }
+
+    public function testUnableToUseNotArrayDataInGenericResponse(): void
+    {
+        $factory = new ApiResponseDataFactory();
+
+        $response = new DataResponse(
+            'success message',
+            200,
+            'Testing phase',
+            new ResponseFactory(),
+            $this->createStub(StreamFactoryInterface::class),
+        );
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('The response data must be either null or an array');
+
+        $factory->createFromResponse($response);
     }
 }
