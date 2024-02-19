56ee7a0356e8344c0ae612cf2a77a639eec0dac9
nkondrashov <n.n.kondrashov@gmail.com>
2024-02-19 10:44:15 +0300

    Update annotations for OpenAPI (#191)
    Co-authored-by: nkondrashov <nkondrashov@arti.ru>

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,51 +4,36 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use Psr\Http\Message\ResponseInterface;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 
-/**
- * @OA\Info(title="Yii API application", version="1.0")
- */
+#[OA\Info(title:"Yii API application", version:"1.0")]
 class IndexController
 {
-    /**
-     * @OA\Get(
-     *     path="/",
-     *     summary="Returns info about the API",
-     *     description="",
-     *
-     *     @OA\Response(
-     *          response="200",
-     *          description="Success",
-     *
-     *          @OA\JsonContent(
-     *              allOf={
-     *
-     *                  @OA\Schema(ref="#/components/schemas/Response"),
-     *                  @OA\Schema(
-     *
-     *                      @OA\Property(
-     *                          property="data",
-     *                          type="object",
-     *                          @OA\Property(
-     *                              property="version",
-     *                              type="string",
-     *                              example="3.0"
-     *                          ),
-     *                          @OA\Property(
-     *                              property="author",
-     *                              type="string",
-     *                              example="yiisoft"
-     *                          ),
-     *                      ),
-     *                  ),
-     *              },
-     *          )
-     *    ),
-     * )
-     */
+
+    #[OA\Get(
+        path: "/",
+        description: "",
+        summary: "Returns info about the API",
+        responses: [
+            new OA\Response(
+                response:"200",
+                description:"Success",
+                content: new OA\JsonContent(
+                    allOf: [
+                        new OA\Schema(ref: "#/components/schemas/Response"),
+                        new OA\Schema(properties: [
+                            new OA\Property(property: "data", properties: [
+                                new OA\Property(property: "version", type: "string", example: "3.0"),
+                                new OA\Property(property: "author", type: "string", example: "yiisoft"),
+                            ], type: "object"),
+                        ]),
+                    ]
+                ),
+            ),
+        ]
+    )]
     public function index(DataResponseFactoryInterface $responseFactory): ResponseInterface
     {
         return $responseFactory->createResponse(['version' => '3.0', 'author' => 'yiisoft']);
