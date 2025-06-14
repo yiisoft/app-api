571f7840887da6074b8a521e33e2ff7a2defde7d
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-06-14 19:22:34 +0300

    Replace "Yii Testing" to "Yii HTTP Runner" usage

--- tests/Support/FunctionalTester.php
+++ tests/Support/FunctionalTester.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Support;
 
 use Codeception\Actor;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
+
+use function dirname;
 
 /**
  * Inherited Methods
@@ -29,4 +34,20 @@ class FunctionalTester extends Actor
     /**
      * Define custom actions here
      */
+    public function sendRequest(ServerRequestInterface $request): ResponseInterface
+    {
+        $runner = new HttpApplicationRunner(
+            rootPath: dirname(__DIR__, 2),
+            environment: $_ENV['YII_ENV'],
+        );
+
+        $response = $runner->runAndGetResponse($request);
+
+        $body = $response->getBody();
+        if ($body->isSeekable()) {
+            $body->rewind();
+        }
+
+        return $response;
+    }
 }
