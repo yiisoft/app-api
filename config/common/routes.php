0f6aec2045f7899588256b7ea979f9e7bb7fd480
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-29 16:30:06 +0300

    Remove Swagger and Yii Debug (#227)

--- config/common/routes.php
+++ config/common/routes.php
@@ -3,13 +3,7 @@
 declare(strict_types=1);
 
 use App\Controller\IndexController;
-use Yiisoft\DataResponse\Middleware\FormatDataResponseAsHtml;
-use Yiisoft\DataResponse\Middleware\FormatDataResponseAsJson;
-use Yiisoft\HttpMiddleware\CorsAllowAllMiddleware;
-use Yiisoft\Router\Group;
 use Yiisoft\Router\Route;
-use Yiisoft\Swagger\Action\SwaggerJson;
-use Yiisoft\Swagger\Action\SwaggerUi;
 
 /**
  * @var array $params
@@ -19,15 +13,4 @@ return [
     Route::get('/')
         ->action([IndexController::class, 'index'])
         ->name('app/index'),
-
-    Group::create('/docs')
-        ->routes(
-            Route::get('')
-                ->middleware(FormatDataResponseAsHtml::class)
-                ->action(fn(SwaggerUi $swaggerUi) => $swaggerUi->withJsonUrl('/docs/openapi.json')),
-            Route::get('/openapi.json')
-                ->middleware(FormatDataResponseAsJson::class)
-                ->middleware(CorsAllowAllMiddleware::class)
-                ->action(fn(SwaggerJson $swaggerJson) => $swaggerJson->withAnnotationPaths(...$params['yiisoft/yii-swagger']['annotation-paths'])),
-        ),
 ];
