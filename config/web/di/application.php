0e7962a408939c861dd633715c16f87b72e317a8
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-31 12:06:43 +0300

    middlewares

--- config/web/di/application.php
+++ config/web/di/application.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 use App\Http\NotFoundHandler;
 use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\Definitions\Reference;
+use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
 use Yiisoft\Injector\Injector;
 use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
+use Yiisoft\RequestProvider\RequestCatcherMiddleware;
+use Yiisoft\Router\Middleware\Router;
+use Yiisoft\Yii\Middleware\Subfolder;
 
 /** @var array $params */
 
@@ -15,7 +19,12 @@ return [
         '__construct()' => [
             'dispatcher' => DynamicReference::to(static function (Injector $injector) use ($params) {
                 return $injector->make(MiddlewareDispatcher::class)
-                    ->withMiddlewares($params['middlewares']);
+                    ->withMiddlewares([
+                        RequestCatcherMiddleware::class,
+                        ErrorCatcher::class,
+                        Subfolder::class,
+                        Router::class,
+                    ]);
             }),
             'fallbackHandler' => Reference::to(NotFoundHandler::class),
         ],
--- config/web/params.php
+++ config/web/params.php
@@ -2,22 +2,10 @@
 
 declare(strict_types=1);
 
-use Yiisoft\ErrorHandler\Middleware\ErrorCatcher;
-use Yiisoft\RequestProvider\RequestCatcherMiddleware;
-use Yiisoft\Router\Middleware\Router;
-use Yiisoft\Yii\Middleware\Subfolder;
-
 return [
     'yiisoft/input-http' => [
         'requestInputParametersResolver' => [
             'throwInputValidationException' => true,
         ],
-    ],
-
-    'middlewares' => [
-        RequestCatcherMiddleware::class,
-        ErrorCatcher::class,
-        Subfolder::class,
-        Router::class,
     ],
 ];
