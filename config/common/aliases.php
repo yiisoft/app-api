0cd5b8a532b5cbc0306a9e5d351cfddc99a8d871
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-08-31 12:11:42 +0300

    aliases

---
+++ config/common/aliases.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    '@root' => dirname(__DIR__, 2),
+    '@assets' => '@public/assets',
+    '@assetsUrl' => '@baseUrl/assets',
+    '@baseUrl' => '/',
+    '@data' => '@root/data',
+    '@messages' => '@resources/messages',
+    '@public' => '@root/public',
+    '@resources' => '@root/resources',
+    '@runtime' => '@root/runtime',
+    '@src' => '@root/src',
+    '@tests' => '@root/tests',
+    '@views' => '@root/views',
+    '@vendor' => '@root/vendor',
+];
--- config/common/params.php
+++ config/common/params.php
@@ -6,21 +6,7 @@ return [
     'supportEmail' => 'support@example.com',
 
     'yiisoft/aliases' => [
-        'aliases' => [
-            '@root' => dirname(__DIR__, 2),
-            '@assets' => '@public/assets',
-            '@assetsUrl' => '@baseUrl/assets',
-            '@baseUrl' => '/',
-            '@data' => '@root/data',
-            '@messages' => '@resources/messages',
-            '@public' => '@root/public',
-            '@resources' => '@root/resources',
-            '@runtime' => '@root/runtime',
-            '@src' => '@root/src',
-            '@tests' => '@root/tests',
-            '@views' => '@root/views',
-            '@vendor' => '@root/vendor',
-        ],
+        'aliases' => require __DIR__ . '/aliases.php',
     ],
 
     'yiisoft/router-fastroute' => [
