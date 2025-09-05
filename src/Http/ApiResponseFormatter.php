a13153aa0ec5a241079a1bad028dd6c432f07e25
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-05 16:07:26 +0300

    Refactor `ApiResponseFormatter` (#235)

--- src/Http/ApiResponseFormatter.php
+++ src/Http/ApiResponseFormatter.php
@@ -4,23 +4,57 @@ declare(strict_types=1);
 
 namespace App\Http;
 
+use LogicException;
 use Psr\Http\Message\ResponseInterface;
 use Yiisoft\DataResponse\DataResponse;
 use Yiisoft\DataResponse\DataResponseFormatterInterface;
 use Yiisoft\DataResponse\Formatter\JsonDataResponseFormatter;
+use Yiisoft\Http\Status;
 
-final class ApiResponseFormatter implements DataResponseFormatterInterface
+use function sprintf;
+
+final readonly class ApiResponseFormatter implements DataResponseFormatterInterface
 {
-    public function __construct(private ApiResponseDataFactory $apiResponseDataFactory, private JsonDataResponseFormatter $jsonDataResponseFormatter) {}
+    public function __construct(
+        private JsonDataResponseFormatter $jsonDataResponseFormatter,
+    ) {}
 
     public function format(DataResponse $dataResponse): ResponseInterface
     {
-        $response = $dataResponse->withData(
-            $this->apiResponseDataFactory
-                ->createFromResponse($dataResponse)
-                ->toArray(),
-        );
+        $data = $dataResponse->getStatusCode() === Status::OK
+            ? $this->createSuccessData($dataResponse)
+            : $this->createFailedData($dataResponse);
 
-        return $this->jsonDataResponseFormatter->format($response);
+        return $this->jsonDataResponseFormatter->format(
+            $dataResponse->withData($data),
+        );
+    }
+
+    private function createSuccessData(DataResponse $response): array
+    {
+        $data = $response->getData();
+        if ($data !== null && !is_array($data)) {
+            throw new LogicException(
+                sprintf(
+                    'The response data must be either null or an array. Got %s.',
+                    get_debug_type($data),
+                ),
+            );
+        }
+
+        return [
+            'status' => 'success',
+            'data' => $data,
+        ];
+    }
+
+    private function createFailedData(DataResponse $response): array
+    {
+        $data = $response->getData();
+        return [
+            'status' => 'failed',
+            'error_message' => is_string($data) && $data !== '' ? $data : 'Unknown error',
+            'error_code' => $response->getStatusCode(),
+        ];
     }
 }
