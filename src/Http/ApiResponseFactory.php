5581cfed780e7a224f3ded2055e8717373f4dd35
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-09-10 15:24:38 +0300

    Add content negotiator

--- src/Http/ApiResponseFactory.php
+++ src/Http/ApiResponseFactory.php
@@ -6,20 +6,20 @@ namespace App\Http;
 
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
-use Yiisoft\Http\Header;
+use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\Http\Status;
-use Yiisoft\Json\Json;
 use Yiisoft\Validator\Result;
 
 final readonly class ApiResponseFactory
 {
     public function __construct(
+        private DataResponseFactoryInterface $dataResponseFactory,
         private ResponseFactoryInterface $responseFactory,
     ) {}
 
     public function success(array|null $data = null): ResponseInterface
     {
-        return $this->createResponse(['status' => 'success', 'data' => $data]);
+        return $this->dataResponseFactory->createResponse(['status' => 'success', 'data' => $data]);
     }
 
     public function fail(
@@ -38,7 +38,7 @@ final readonly class ApiResponseFactory
         if ($data !== null) {
             $result['error_data'] = $data;
         }
-        return $this->createResponse($result, $httpCode);
+        return $this->dataResponseFactory->createResponse($result, $httpCode);
     }
 
     public function notFound(string $message = 'Not found.'): ResponseInterface
@@ -53,14 +53,5 @@ final readonly class ApiResponseFactory
             $result->getErrorMessagesIndexedByPath(),
             httpCode: Status::UNPROCESSABLE_ENTITY,
         );
-    }
-
-    private function createResponse(array $data, int $code = Status::OK): ResponseInterface
-    {
-        $response = $this->responseFactory
-            ->createResponse($code)
-            ->withHeader(Header::CONTENT_TYPE, "application/json; charset=UTF-8");
-        $response->getBody()->write(Json::encode($data));
-        return $response;
     }
 }
